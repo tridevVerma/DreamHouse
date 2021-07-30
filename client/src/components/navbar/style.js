bfd265f125dae006b5afa33bcf7db7fd14ed9c30
Tridev Verma <tv072000@gmail.com>
2021-07-30 21:53:35 +0530

    applying  material ui bug fix

--- client/src/components/navbar/style.js
+++ client/src/components/navbar/style.js
@@ -1,67 +1,70 @@
 import { makeStyles } from "@material-ui/core";
 
-export const useStyles = makeStyles((theme) => ({
-  nav: {
-    paddingTop: "0rem",
-    paddingBottom: "0rem",
-    height: "80px",
-    zIndex: theme.zIndex.appBar,
-    backgroundColor: "white",
-    [theme.breakpoints.down(theme.breakpoints.values.md + 32)]: {
-      display: "none",
+export const useStyles = makeStyles(
+  (theme) => ({
+    nav: {
+      paddingTop: "0rem",
+      paddingBottom: "0rem",
+      height: "80px",
+      zIndex: theme.zIndex.appBar,
+      backgroundColor: "white",
+      [theme.breakpoints.down(theme.breakpoints.values.md + 32)]: {
+        display: "none",
+      },
     },
-  },
-  logoName: {
-    [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
-      fontSize: "1.8rem",
+    logoName: {
+      [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
+        fontSize: "1.8rem",
+      },
     },
-  },
 
-  linksUL: {
-    [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
-      width: "100% !important",
+    linksUL: {
+      [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
+        width: "100% !important",
+      },
     },
-  },
 
-  navItem: {
-    display: "flex",
-    alignItems: "center",
-    color: "white",
-    margin: "1rem",
-    borderRadius: "4px",
-    "&:hover": {
-      color: "black",
+    navItem: {
+      display: "flex",
+      alignItems: "center",
+      color: "white",
+      margin: "1rem",
+      borderRadius: "4px",
+      "&:hover": {
+        color: "black",
+      },
     },
-  },
-  navLinks: {
-    fontWeight: "bold",
-    textDecoration: "none",
-    color: "inherit",
-    "&:hover": {
-      color: "black",
+    navLinks: {
+      fontWeight: "bold",
+      textDecoration: "none",
+      color: "inherit",
+      "&:hover": {
+        color: "black",
+      },
+      "&:focus": {
+        color: "black",
+      },
     },
-    "&:focus": {
-      color: "black",
+    moreIcon: {
+      fontSize: "1.2rem",
+      fontWeight: "bold",
+      color: "inherit",
     },
-  },
-  moreIcon: {
-    fontSize: "1.2rem",
-    fontWeight: "bold",
-    color: "inherit",
-  },
-  myBtn: {
-    fontSize: "14px",
-    backgroundColor: "#3454d1",
-    padding: "0.75rem 2rem",
-    color: "white",
-    fontWeight: "bold",
-    border: "none",
-    borderRadius: "2px",
-    "&:hover": {
-      backgroundColor: "#2a43a7",
+    myBtn: {
+      fontSize: "14px",
+      backgroundColor: "#3454d1",
+      padding: "0.75rem 2rem",
+      color: "white",
+      fontWeight: "bold",
+      border: "none",
+      borderRadius: "2px",
+      "&:hover": {
+        backgroundColor: "#2a43a7",
+      },
+      [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
+        padding: "0.5rem 1rem",
+      },
     },
-    [theme.breakpoints.down(theme.breakpoints.values.md + 181)]: {
-      padding: "0.5rem 1rem",
-    },
-  },
-}));
+  }),
+  { index: 1 }
+);
