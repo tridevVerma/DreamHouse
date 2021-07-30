88d9fca4597aa04c3555dd201d99eb9bac5d86eb
Tridev Verma <tv072000@gmail.com>
2021-07-31 04:22:35 +0530

    checking material ui fix

--- client/src/components/drawer/styles.js
+++ client/src/components/drawer/styles.js
@@ -1,31 +1,34 @@
 import { makeStyles } from "@material-ui/core";
 
-export const useStyles = makeStyles((theme) => ({
-  drawer: {
-    zIndex: theme.zIndex.appBar,
-    boxShadow: "none",
-    [theme.breakpoints.up(theme.breakpoints.values.md + 32)]: {
-      display: "none",
+export const useStyles = makeStyles(
+  (theme) => ({
+    drawer: {
+      zIndex: theme.zIndex.appBar,
+      boxShadow: "none",
+      [theme.breakpoints.up(theme.breakpoints.values.md + 32)]: {
+        display: "none",
+      },
     },
-  },
-  sidebar: {
-    width: "200px",
-    [theme.breakpoints.up("sm")]: {
-      width: "250px",
+    sidebar: {
+      width: "200px",
+      [theme.breakpoints.up("sm")]: {
+        width: "250px",
+      },
     },
-  },
-  logoName: {
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "1.5rem",
+    logoName: {
+      [theme.breakpoints.down("sm")]: {
+        fontSize: "1.5rem",
+      },
     },
-  },
-  links: {
-    textDecoration: "none",
-    width: "100%",
-    fontSize: "1.2rem",
-    color: "black",
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
+    links: {
+      textDecoration: "none",
+      width: "100%",
+      fontSize: "1.2rem",
+      color: "black",
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+  }),
+  { index: 1 }
+);
